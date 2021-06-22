06a18a3a1ecc93b54e301c6846720d9b292d44f6
Yashan Bansal <71026956+yashanbansal@users.noreply.github.com>
2021-06-22 13:53:28 +0530

    initial commit

---
+++ delete-inline.php
@@ -0,0 +1,14 @@
+<?php
+
+echo $stu_id = $_GET['id'];
+
+   include 'config.php';
+
+   $sql = "DELETE FROM student WHERE sid={$stu_id}";
+   $result = mysqli_query($conn , $sql) or die("Query unsuccessfull!!");
+
+   header("Location: http://localhost/crud/index.php");
+
+   mysqli_close($conn);
+
+?>
